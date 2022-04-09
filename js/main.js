1b84b7cf6dc3d6ceb790db6bcf0b1a8583a8dc60
AKRAM <mami.akram@live.fr>
2022-04-09 15:57:06 +0100

    index page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
- 
+
+$(document).ready(function(){
+
+    $("#main-carousel").on('slide.bs.carousel', function(event){
+        
+        
+       var target=$(event.relatedTarget).find('.carousel-item-content');
+       
+        $("#slider-full-width").css("background-image", `url('${ target.data('image-url')}')`); 
+    }); 
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+         
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+});
